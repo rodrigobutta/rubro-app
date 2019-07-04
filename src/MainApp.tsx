7632db7f6eeb52148ea1e3477c4b3577d7828b41
rodrigobutta <rbutta@gmail.com>
2019-07-04 11:31:28 -0300

    sigue funcionando y agregue libs en build de pusher y firebase messagin

--- src/MainApp.tsx
+++ src/MainApp.tsx
@@ -24,15 +24,15 @@ import Pusher from 'pusher-js/react-native';
 // Enable pusher logging - don't include this in production
 Pusher.logToConsole = true;
 
-var pusher = new Pusher('337ed7b75cb40c3bc159', {
-  cluster: 'us2',
-  forceTLS: true
-});
+// var pusher = new Pusher('337ed7b75cb40c3bc159', {
+//   cluster: 'us2',
+//   forceTLS: true
+// });
 
-var channel = pusher.subscribe('my-channel');
-channel.bind('my-event', function(data) {
-  alert(JSON.stringify(data));
-});
+// var channel = pusher.subscribe('my-channel');
+// channel.bind('my-event', function(data) {
+//   alert(JSON.stringify(data));
+// });
 
 // FINPUSHER
 
